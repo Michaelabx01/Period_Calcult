d5c42f682dcd530caa1a84a23d44c71ccc21123b
michael_abx <mavaldiviezo.abx@gmail.com>
2023-11-14 17:27:05 -0500

    Initial commit

---
+++ app/src/main/java/com/mvaldiviezoutp/period_calcult/MenuPrincipal.java
@@ -0,0 +1,42 @@
+package com.mvaldiviezoutp.period_calcult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    Button CerrarSesion;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+        CerrarSesion = findViewById(R.id.CerrarSesion);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SalirAplicacion();
+            }
+        });
+    }
+
+    private void SalirAplicacion() {
+        firebaseAuth.signOut();
+        startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+        Toast.makeText(this, "Cerraste sesión exitosamente", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/mvaldiviezoutp/period_calcult/Registro.java
@@ -0,0 +1,169 @@
+package com.mvaldiviezoutp.period_calcult;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+
+public class Registro extends AppCompatActivity {
+
+    EditText NombreEt,CorreoEt,ContaseñaEt,ConfirmarContraseñaEt;
+    Button RegistrarUsuario;
+    TextView TengounacuentaTXT;
+
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+
+    //
+    String nombre = " " , correo = " ", password = "" , confirmarpassword = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("Registrar");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        NombreEt = findViewById(R.id.NombreEt);
+        CorreoEt = findViewById(R.id.CorreoEt);
+        ContaseñaEt = findViewById(R.id.ContaseñaEt);
+        ConfirmarContraseñaEt = findViewById(R.id.ConfirmarContraseñaEt);
+        RegistrarUsuario = findViewById(R.id.RegistrarUsuario);
+        TengounacuentaTXT = findViewById(R.id.TengounacuentaTXT);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(Registro.this);
+        progressDialog.setTitle("Espere por favor");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        RegistrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+                ValidarDatos();
+            }
+        });
+
+        TengounacuentaTXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Registro.this, Login.class));
+            }
+        });
+    }
+
+    private void ValidarDatos(){
+        nombre = NombreEt.getText().toString();
+        correo = CorreoEt.getText().toString();
+        password = ContaseñaEt.getText().toString();
+        confirmarpassword = ConfirmarContraseñaEt.getText().toString();
+
+        if (TextUtils.isEmpty(nombre)){
+            Toast.makeText(this, "Ingrese nombre", Toast.LENGTH_SHORT).show();
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+            Toast.makeText(this, "Ingrese correo", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Ingrese contraseña", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (TextUtils.isEmpty(confirmarpassword)){
+            Toast.makeText(this, "Confirme contraseña", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (!password.equals(confirmarpassword)){
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            CrearCuenta();
+        }
+    }
+
+    private void CrearCuenta() {
+        progressDialog.setMessage("Creando su cuenta...");
+        progressDialog.show();
+
+        //Crear un usuario en Firebase
+        firebaseAuth.createUserWithEmailAndPassword(correo, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //
+                        GuardarInformacion();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(Registro.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void GuardarInformacion() {
+        progressDialog.setMessage("Guardando su información");
+        progressDialog.dismiss();
+
+        //Obtener la identificación de usuario actual
+        String uid = firebaseAuth.getUid();
+
+        HashMap<String, String> Datos = new HashMap<>();
+        Datos.put("uid",  uid);
+        Datos.put("correo", correo);
+        Datos.put("nombres", nombre);
+        Datos.put("password", password);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Usuarios");
+        databaseReference.child(uid)
+                .setValue(Datos)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Toast.makeText(Registro.this, "Cuenta creada con éxito", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Registro.this, MenuPrincipal.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull  Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(Registro.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
